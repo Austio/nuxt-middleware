3407aabfc5b85982bfb4439be328bdbbe7aae505
Austin Story <lonnieastory@gmail.com>
2019-05-17 07:17:27 -0500

    Add promise middleware to show it will await before continuing

--- middleware/routeMiddleware.js
+++ middleware/routeMiddleware.js
@@ -1,3 +1,11 @@
 export default function ({ isClient, route }) {
   console.log('routeMiddleware', `IsClient: ${isClient}`, `route: ${route.path}`);
+
+  return new Promise((resolve, reject) => {
+    console.log('routerMiddleware Promise')
+    setTimeout(() => {
+      console.log('routerMiddleware Promise complete')
+      resolve();
+    }, 5000)
+  })
 }
